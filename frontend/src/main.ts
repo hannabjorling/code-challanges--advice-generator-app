91931f2d58304a920743c07a7cd97ba95e6c116d
Hanna Björling <hanna.bjorling@hotmail.com>
2022-12-14 13:57:39 +0100

    final

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '/styles/style.css'
 
+const quoteEl = document.querySelector('p')!
+const adviseEl = document.querySelector('span')!
+const nextBtn = document.querySelector('button')
+
+interface Quote {
+  id: number,
+  advice: string,
+}
+
+let quotes: Quote[] = []
+
+const fetchQuotes = async () => {
+  const response = await fetch('https://api.adviceslip.com/advice')
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`)
+  }
+
+  return await response.json() as Quote[]
+}
+
+const getQuotes = async () => {
+  quotes = await fetchQuotes()
+
+  renderQuotes()
+}
+
+const renderQuotes = () => {
+  quoteEl.innerText =
+    Object.values(quotes)
+      .map((quote) => `"${quote.advice}"`)
+      .join('')
+
+  adviseEl.innerText =
+    Object.values(quotes)
+      .map((quote) => ` #${quote.id}`)
+      .join('')
+}
+
+nextBtn?.addEventListener('click', () => {
+  getQuotes()
+})
+
+getQuotes()
